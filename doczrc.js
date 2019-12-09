504350b9db3f4cab33f052a13ec99b58c3279f33
Marc Garreau <marcdgarreau@gmail.com>
2019-12-09 13:34:00 -0700

    test

--- doczrc.js
+++ doczrc.js
@@ -1,6 +1,7 @@
 export default {
-  title: 'Ethereum Foundation JavaScript Team',
+  title: 'EF JavaScript Team',
   description: 'A guide to projects maintained by the EF JS team',
   files: './website/**/*.{md,mdx}',
+  base: '/js-organization/',
   dest: '/docs'
 }
